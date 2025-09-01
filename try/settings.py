0ec1be6e8fa127a54ecccc0e9b68632c9c0fa3ed
Priyanshu <167947406+King-Priyanshu@users.noreply.github.com>
2025-09-01 16:07:38 +0530

    Update settings.py

--- try/settings.py
+++ try/settings.py
@@ -133,8 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'  # Use an app password for Gmail
+EMAIL_HOST_USER = 'kingprincepriyanshu1138@gmail.com'
+EMAIL_HOST_PASSWORD = 'glshspdxruthmnte'
